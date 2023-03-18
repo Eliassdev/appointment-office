4e95ea3be3efe6bbd2e8640f17d9783060cc3b77
El Bebelo <nacho71197@gmail.com>
2023-03-17 22:18:09 -0300

    Reparacion en importaciones

--- src/components/StylistsForm/StylistTable.jsx
+++ src/components/StylistsForm/StylistTable.jsx
@@ -1,84 +1,77 @@
-import React,{useState, useEffect} from 'react'
-import {stylists} from "./data.json"
+import React, { useEffect, useState } from 'react';
+import { stylists } from './data.json';
 
+const StylistTable = () => {
+  const [stylist, setStylist] = useState('');
+  console.log(stylists);
+  // const StylistData = async() =>{
+  //     const data = await fetch("https://appointment-organizations-production.up.railway.app/v1/stylists", {
+  //         method: "GET",
+  //         mode: "no-cors",
+  //     })
+  //     const res = await data.json();
+  //     console.log(res)
+  //         setstylist(res)
 
+  // }
+  // async function StylistData(){
+  //     try{
+  //         let res = await fetch("https://appointment-organizations-production.up.railway.app/v1/stylists", {method: "GET",mode: "no-cors"});
+  //         res= res.json();
+  // //         setStylist(res);
+  // //     }catch (err){
+  // //         console.log("ERROR: ", err);
+  // //     }
+  // // }
 
-export const StylistTable = () => {
-    const [stylist, setStylist] = useState("")
-    console.log(stylists);
-    // const StylistData = async() =>{
-    //     const data = await fetch("https://appointment-organizations-production.up.railway.app/v1/stylists", {
-    //         method: "GET",
-    //         mode: "no-cors",
-    //     })
-    //     const res = await data.json();
-    //     console.log(res)
-    //         setstylist(res)
-       
-    // } 
-    // async function StylistData(){
-    //     try{
-    //         let res = await fetch("https://appointment-organizations-production.up.railway.app/v1/stylists", {method: "GET",mode: "no-cors"});
-    //         res= res.json();
-    // //         setStylist(res);
-    // //     }catch (err){
-    // //         console.log("ERROR: ", err);
-    // //     }
-    // // }
+  // useEffect(() => {
 
-    // useEffect(() => {
+  //   if(stylist === ""){
+  //     StylistData()
+  //   }
 
-    //   if(stylist === ""){
-    //     StylistData()
-    //   }
-    
-    // }, [stylist])
-    
-    console.log(stylists);
+  // }, [stylist])
+
+  console.log(stylists);
   return (
-    <div>StylistTable
-        <table>
-            
-            <thead>
-                <tr>
-                    <td>sti.address</td>
-                    <td>sti.city</td>
-                    <td>sti.country</td>
-                    <td>sti.gender</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-            </thead>
-            <tbody>
-                {stylists.map((sti) => {
-                    console.log(sti)
-                    return(
+    <div>
+      StylistTable
+      <table>
+        <thead>
+          <tr>
+            <td>sti.address</td>
+            <td>sti.city</td>
+            <td>sti.country</td>
+            <td>sti.gender</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </thead>
+        <tbody>
+          {stylists.map((sti) => {
+            console.log(sti);
+            return (
+              <tr>
+                <td>{sti.stylist_firstname} </td>
 
-                        <tr>
-                        <td>{sti.stylist_firstname} </td>
-                        
-                        <td>{sti.address} </td>
-                        <td>{sti.city} </td>
-                        <td>{sti.country} </td>
-                        <td>{sti.gender} </td>
-                        {/* <td>{sti} </td>
+                <td>{sti.address} </td>
+                <td>{sti.city} </td>
+                <td>{sti.country} </td>
+                <td>{sti.gender} </td>
+                {/* <td>{sti} </td>
                         <td>{sti} </td>
                         <td>{sti} </td>
                         <td>{sti} </td>  */}
-                    </tr>
-                    )       
-                })}
-            </tbody>
-                
-                
-            
-        </table>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
 
-
-
+export default StylistTable;
