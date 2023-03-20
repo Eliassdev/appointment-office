4721b600ddd6fc9904872e54c132868a88a4ad9d
Elias Flores <elias.leo.flores@gmail.com>
2023-03-20 18:07:53 -0300

    trabajo en tabla stylist

--- src/components/StylistsForm/StylistTable.jsx
+++ src/components/StylistsForm/StylistTable.jsx
@@ -7,41 +7,46 @@ const StylistTable = () => {
   console.log(stylists);
 
   return (
-    <div>
-      StylistTable
-      <table>
-        <thead>
-          <tr>
-            <td>sti.address</td>
-            <td>sti.city</td>
-            <td>sti.country</td>
-            <td>sti.gender</td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-        </thead>
-        <tbody>
-          {stylists?.map((sti) => {
-            console.log(sti);
-            return (
-              <tr>
-                <td>{sti.stylist_firstname} </td>
-                <td>{sti.address} </td>
-                <td>{sti.city} </td>
-                <td>{sti.country} </td>
-                <td>{sti.gender} </td>
-                {/* // <td>{sti} </td>
-                // <td>{sti} </td>
-                // <td>{sti} </td>
-                // <td>{sti} </td> */}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+    <div>StylistTable
+        <table>
+            <thead>
+                <tr>
+                    <td>Nombre</td>
+                    <td>Address</td>
+                    <td>City</td>
+                    <td>Country</td>
+                    <td>Gender</td>
+                    {/* <td></td>
+                    <td></td>
+                    <td></td> */}
+                    <td>Marital Status</td>
+                </tr>
+            </thead>
+            <tbody>
+                  {stylists.map((sti) => {
+                    console.log(sti)
+                    return(
+
+                        <tr>
+                        <td>{sti.stylist_firstname} </td>
+                        <td>{sti.address} </td>
+                        <td>{sti.city} </td>
+                        <td>{sti.country} </td>
+                        <td>{sti.gender} </td>
+                        {/* <td>{sti.is_active} </td> */}
+                        {/* <td>{sti.latitude} </td>
+                        <td>{sti.longitude} </td> */}
+                        <td>{sti.marital_status} </td>
+                        <button type='submit'>Upgrade </button>
+                        <button type='reset'>Delete </button>
+                    </tr>
+                    )       
+                })}
+            </tbody>
+                
+                
+            
+        </table>
     </div>
   );
 };
