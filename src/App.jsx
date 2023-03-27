a0d524fd1087540c1910e1a2f27b5f449788c4f6
Roy Lopez <roylopezdev@gmail.com>
2023-03-27 10:28:41 -0300

    add organizations and register to app path

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 //Local Imports
-import './index.css';
-import Dashboard from './pages/Dashboard';
-import Home from './pages/Home';
-import Register from './pages/Register';
+import "./index.css";
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import OrganizationsDashboard from "./components/Organizations/OrganizationsDashboard.component";
+import OrganizationForm from "./components/Organizations/OrganizacionForm.component";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route path="organizations" element={<OrganizationsDashboard />}>
+            <Route path="register" element={<OrganizationForm />} />
+          </Route>
+        </Route>
         <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
