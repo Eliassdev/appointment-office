a3a553e90b9a7ef123eabeb16d25b1bbba5697ff
Roy Lopez <roylopezdev@gmail.com>
2023-03-31 16:08:41 -0300

    create useCountryState.hook

--- src/hooks/useCountryState.hook.jsx
+++ src/hooks/useCountryState.hook.jsx
@@ -4,7 +4,11 @@ import { Country, State } from 'country-state-city';
 const useCountryState = () => {
   const [countries, setCountries] = useState([]);
   const [states, setStates] = useState([]);
+<<<<<<< HEAD
   const [selectedCountry, setSelectedCountry] = useState(null);
+=======
+  const [selectedCountry, setSelectedCountry] = useState('');
+>>>>>>> d239a85 (create useCountryState.hook)
 
   useEffect(() => {
     setCountries(Country.getAllCountries());
@@ -12,6 +16,7 @@ const useCountryState = () => {
 
   useEffect(() => {
     if (selectedCountry) {
+<<<<<<< HEAD
       setStates(State.getStatesOfCountry(selectedCountry.isoCode));
     }
   }, [selectedCountry]);
@@ -34,6 +39,20 @@ const useCountryState = () => {
     // Get selected country
     selectedCountry,
     // Handle country change
+=======
+      setStates(State(selectedCountry));
+    }
+  }, [selectedCountry]);
+
+  const handleCountryChange = (event) => {
+    setSelectedCountry(event.target.value);
+  };
+
+  return {
+    countries,
+    states,
+    selectedCountry,
+>>>>>>> d239a85 (create useCountryState.hook)
     handleCountryChange,
   };
 };
