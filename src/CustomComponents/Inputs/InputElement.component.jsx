cfad6443b6f73d21a20deedd02b3ee366f7d9dda
Roy Lopez <roylopezdev@gmail.com>
2023-04-02 16:48:46 -0300

    create InputElement component

---
+++ src/CustomComponents/Inputs/InputElement.component.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+function InputElement({
+  title,
+  value,
+  onChange,
+  onBlur,
+  name,
+  type,
+  id,
+  touched,
+  errors,
+}) {
+  return (
+    <div className="mb-2 flex flex-col px-4">
+      <label className="mb-1 text-neutral-100" htmlFor="short_name">
+        {title}
+      </label>
+      <input
+        name={name}
+        type={type}
+        id={id}
+        className=" h-8 w-full rounded border-transparent bg-neutral-700  p-2 text-white outline-2 outline-transparent ring-2 ring-transparent focus:border-purple-500 focus:outline-purple-500 focus:ring-purple-500"
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+      />
+      {touched && errors ? (
+        <span className="text-red-600">{errors}</span>
+      ) : null}
+    </div>
+  );
+}
+
+export default InputElement;
